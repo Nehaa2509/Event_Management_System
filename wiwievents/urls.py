55c6dbfca4e68f4f5c4c1e30a5d123c2595b1400
Nehaa2509 <221210116040@aiet.edu.in>
2026-07-26 23:52:47 +0530

    Add temporary admin setup URL endpoint

--- wiwievents/urls.py
+++ wiwievents/urls.py
@@ -5,9 +5,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from events.admin_site import wiwi_admin_site
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def create_admin(request):
+    u, created = User.objects.get_or_create(username='sneha')
+    u.is_staff = True
+    u.is_superuser = True
+    u.set_password('admin123')
+    u.save()
+    return HttpResponse("SUCCESS! The user 'sneha' is now a superuser. You can log into the admin page with password 'admin123'.")
 
 urlpatterns = [
     path('admin/', wiwi_admin_site.urls),
+    path('setup-admin-999/', create_admin),
     path('api/v1/', include('events.api_urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('events.urls')),
